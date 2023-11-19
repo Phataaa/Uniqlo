55af6d389785b649ac6602cffc41ff5fce43dbea
Phataaa <dphat042003@gmail.com>
2023-11-20 06:37:10 +0700

    update(2nd)

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -10,18 +10,14 @@ class Cart extends Model
     use HasFactory;
     protected $primaryKey = 'id';
     protected $table = 'cart';
-    protected $fillable = ['amount', 'total', 'order_id', 'product_id', 'user_id'];
+    protected $fillable = ['amount', 'total', 'product_id', 'user_id'];
 
     
-    public function orders() {
-        return $this-> belongsTo('App\Models\Order', 'order_id');
-    }
-
-    public function product() {
-        return $this -> belongsTo('App\Models\Product', 'product_id');
-    }
     public function user() {
         return $this -> belongsTo('App\Models\User', 'user_id');
+    }
+    public function cartDetail() {
+        return $this -> hasMany('App/Models/CartDetail');
     }
 
 }
